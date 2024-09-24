ea22756dfa312dc1ed4b463d243165c1d814511d
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-09-25 01:17:45 +0300

    Created a Hamburger element that drops down  the section.

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const toggleButton = document.getElementById('toggle-dark-mode');
 const body = document.body;
 const content = document.getElementById("content");
+const hamburger = document.getElementById('hamburger-menu');
+const navMenu = document.getElementById('nav-menu');
 
-setTimeout(function() {
-    content.style.display = 'block';
-}, 2000);
+window.onload = function() {
+  setTimeout(() => {
+    document.getElementById('content').style.display = 'block';
+  }, 2000);
+};
 
+hamburger.addEventListener('click', () => {
+  // console.log('Hamburger clicked!');
+  navMenu.classList.toggle('show');
+  const expanded = navMenu.classList.contains('show');
+  hamburger.setAttribute('aria-expanded', expanded);
+});
+
+
+if (localStorage.getItem('dark-mode') === 'enabled') {
+  body.classList.add('dark-mode');
+}
 
 toggleButton.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('dark-mode', 'enabled');
+} else {
+    localStorage.setItem('dark-mode', 'disabled');
+}
 });
